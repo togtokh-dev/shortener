a6d2c3070f0528c11d0fab44caf0403c3fc392a7
CORP\buyantogtokh.alt <togtokh.dev@gmail.com>
2022-11-29 01:26:03 +0800

    main

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,16 +6,20 @@ const connectDB = async () => {
   mongoose.Promise = bluebird;
   try {
     const conn = await mongoose.connect(mongoUrl, {
+      dbName: process.env.MONGODB_dbName,
       // useNewUrlParser: true,
       // useCreateIndex: true,
       // useUnifiedTopology: true,
       // useFindAndModify: false,
     });
-    console.log(`Data base connected : ${conn.connection.host}`);
+    console.log(
+      `Data base connected : ${conn.connection.host}@${conn.connection.name}`
+    );
   } catch (error) {
     console.log(
       `MongoDB connection error. Please make sure MongoDB is running. ${error}`
     );
+    process.exit(1);
   }
 };
 export default connectDB;
